93e15cc67479752d9146f3f08f7bf553176a4b64
Aman Bhargava <ama.ab36@gmail.com>
2023-03-29 14:48:25 +0530

    optimiztion of database
    all the documents converted to collections

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -128,9 +128,8 @@ router.get( "/collections", function ( req, res ) {
 } )
 
 router.get( "/discounted-items", function ( req, res ) {
-    
-} )
 
+} )
 
 
 
@@ -141,60 +140,31 @@ router.get( "/collections/:product", async function ( req, res ) {
 
 
     // reading all the data of products in db
-    const docSnap = await getDoc( doc( imports.db, "ayruJaipur", product ) );
-    if ( docSnap.exists() ) {
+    const querySnapshot = await getDocs( collection( imports.db, product ) );
 
-        console.table( docSnap.data() );
+    const ProductDataList = await Promise.all( querySnapshot.docs.map( async ( doc ) => {
 
-    } else {
-        console.log( "No such document!" );
-    }
+        const imgRefProduct = ref( imports.storage, product + "/" + doc.id );
+        const imgURLProduct = await getDownloadURL( imgRefProduct )
+            .then( ( url ) => {
+                // `url` is the download URL for 'images/stars.jpg'
 
+                return url;
+            } )
+            .catch( ( er2 ) => {
+                console.error( er2 )
+            } );
 
+        let totalQuantityAvailable = parseInt( doc.data().king[ 1 ] ) + parseInt( doc.data().queen[ 1 ] ) + parseInt( doc.data().single[ 1 ] )
 
-    // reference to a bucket in cloud storage
-    const storageRef = ref( imports.storage, product );
-
-
-    // array of objects of all the images in bucket
-    const itemList = await listAll( storageRef )
-        .then( async ( imgRef ) => {
-
-            const imgArr = await Promise.all( imgRef.items.map( async ( itemRef ) => {
+        return ( { itemId: doc.id, itemName: doc.data().name, itemImgUrl: imgURLProduct, itemPrice: formateCurrency( doc.data().baseprice ), itemAvaibility: totalQuantityAvailable } )
 
 
-                const imgData = await getDownloadURL( itemRef )
-                    .then( async ( imgUrl ) => {
+    } ) );
 
-                        const item = docSnap.data()[ itemRef.name ]
-                        // console.log( itemRef.name + " => " + imgUrl );
-                        // console.table( item );
-                        let totalQuantityAvailable = parseInt( item[ 'king' ][ 1 ] ) + parseInt( item[ 'queen' ][ 1 ] ) + parseInt( item[ 'single' ][ 1 ] )
+    console.log( ProductDataList );
 
-                        return ( { itemId: itemRef.name, itemName: item.name, itemImgUrl: imgUrl, itemPrice: formateCurrency( item.baseprice ), itemAvaibility: totalQuantityAvailable } )
-
-                        // return ( { imgID: itemRef.name, itemImgUrl: imgUrl } )
-
-
-
-                    } )
-                    .catch( ( error ) => {
-                        console.error( error )
-                    } );
-                return imgData;
-
-            } ) )
-            // console.log( imgArr );
-            return imgArr;
-
-
-        } ).catch( ( er2 ) => {
-            console.error( er2 );
-        } );
-
-    console.table( itemList );
-
-    res.render( "products", { list: itemList, product: product } );
+    res.render( "products", { list: ProductDataList, product: product } );
 } )
 
 
@@ -205,15 +175,8 @@ router.get( "/collections/:category/product/:item", async function ( req, res ) 
     const category = req.params.category;
 
     // reading all the data of products in db
-    const docSnap = await getDoc( doc( imports.db, "ayruJaipur", category ) );
-    if ( docSnap.exists() ) {
-        console.table( docSnap.data()[ itemID ] );
-
-    } else {
-        console.log( "No such document!" );
-    }
-
-
+    const itemRef = doc( imports.db, category, itemID );
+    const itemSnap = await getDoc( itemRef );
 
     // reference to a bucket in cloud storage
     const imgRef = ref( imports.storage, category + "/" + itemID );
@@ -228,7 +191,7 @@ router.get( "/collections/:category/product/:item", async function ( req, res ) 
             console.error( er3 )
         } );
 
-    const itemData = { ...docSnap.data()[ itemID ], url: imgURL };
+    const itemData = { ...itemSnap.data(), url: imgURL };
 
     itemData.single[ 0 ] = formateCurrency( itemData.single[ 0 ] )
     itemData.queen[ 0 ] = formateCurrency( itemData.queen[ 0 ] )
@@ -239,7 +202,6 @@ router.get( "/collections/:category/product/:item", async function ( req, res ) 
     res.render( 'item', { itemData: itemData } )
 
 } );
-
 
 
 // refunf policy page
