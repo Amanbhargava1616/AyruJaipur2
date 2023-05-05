d4cbc092ebe2928e60ddb636b7a4d6b16e8e5bcd
Aman Bhargava <ama.ab36@gmail.com>
2023-05-05 23:32:27 +0530

    client love carousel completed
    carousel is autoplay and holds the reviews of the clients

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -7,4 +7,13 @@ $( function () {
         cssEase: 'linear'
     } );
 
+    $( '.autoplayClientLoveSlick' ).slick( {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+        speed:3000
+    } );
+
 } )
